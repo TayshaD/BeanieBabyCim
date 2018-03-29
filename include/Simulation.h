bb1e313a9deb4dee19442b12ecccfd992caf1060
Taysha <taariyadavis@gmail.com>
2018-03-28 23:30:38 -0500

    declared class Simulation

---
+++ include/Simulation.h
@@ -0,0 +1,23 @@
+//
+// Created by taja on 3/28/18.
+//
+
+#ifndef BEANIEBABYSIM_SIMULATION_H
+#define BEANIEBABYSIM_SIMULATION_H
+
+#include <string>
+
+class Simulation {
+
+    int global_time_;
+
+    Simulation();
+
+    std::string get_time_string();
+
+};
+
+
+
+
+#endif //BEANIEBABYSIM_SIMULATION_H
