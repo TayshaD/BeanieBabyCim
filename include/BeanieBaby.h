5ce52bc4b510e0f454aaf60e783cb8258e21272f
Taysha <taariyadavis@gmail.com>
2018-04-10 13:07:40 -0500

    declared useful member functions

--- include/BeanieBaby.h
+++ include/BeanieBaby.h
@@ -33,6 +33,18 @@ public:
 
     BeanieBaby(std::string name, int copies, double base_price = 5);
 
+    void transfer_copies(BeanieBaby& other_baby, int copies_to_transfer);
+
+    bool get_name();
+
+    int get_copies();
+
+    double get_price();
+
+    bool is_retired();
+
+    static std::vector<BeanieBaby> All_Babies;
+
     /**Breaking the rule of three/five: I need a custom copy constructor, because in my case I DON'T want every member
      * to be copied, as in the default. Consider the case where one buys
      * @param other_Baby Baby to be copied.
@@ -59,6 +71,6 @@ public:
 std::string LoadBeanieBabyData();
 
 /**Method to parse data from provided string and return BeanieBaby objects*/
-std::vector<BeanieBaby>  ParseBeanieData(std::string json_string);
+std::vector<BeanieBaby>  ParseBea    boolnieData(std::string json_string);
 
 #endif //BEANIEBABYSIM_BEANIEBABY_H
