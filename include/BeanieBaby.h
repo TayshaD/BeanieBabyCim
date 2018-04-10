2a359b9903e19e3be779924504c74473328521be
Taysha <taariyadavis@gmail.com>
2018-04-09 20:42:13 -0500

    updated BeanieBaby header with member variables

--- include/BeanieBaby.h
+++ include/BeanieBaby.h
@@ -1,4 +1,23 @@
 #ifndef BEANIEBABYSIM_BEANIEBABY_H
 #define BEANIEBABYSIM_BEANIEBABY_H
 
+#include <string>
+
+class {
+    /**Name of the Beanie Baby**/
+    std::string name_;
+
+    /**Starting price of the baby before any adjustments.*/
+    double base_price_;
+
+    /**Essentially how valued/sought after this baby is. A value greater than 1 means it's value is greater than its
+     * base price, a value lower than 1 means its value has depreciated from the base price*/
+    double market_value_;
+
+    /**The price of the baby after its current market value is taken into account*/
+    double market_price_;
+
+    /**When a baby is retired, it is no longer being produced, and its market value increases*/
+    bool retired_;
+};
 #endif //BEANIEBABYSIM_BEANIEBABY_H
--- include/Context.h
+++ include/Context.h
@@ -1,9 +1,8 @@
-//
-// Created by taja on 3/29/18.
-//
-
 #ifndef BEANIEBABYSIM_CONTEXT_H
 #define BEANIEBABYSIM_CONTEXT_H
+
+#include <vector>
+#include <string>
 
 class Context {
 protected:
@@ -12,9 +11,9 @@ protected:
 public:
     int get_time() {return time_;}
 
-    virtual std::vector<string> ExecuteCommand(std::string command) = 0;
+    virtual std::vector<std::string> ExecuteCommand(std::string command) = 0;
 
-    virtual std::vector<string> Help() = 0;
+    virtual std::vector<std::string> Help() = 0;
 
 };
 #endif //BEANIEBABYSIM_CONTEXT_H
