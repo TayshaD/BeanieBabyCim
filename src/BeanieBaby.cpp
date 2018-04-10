53938ef6984b976453a102508dbf6e27f6be34c0
Taysha <taariyadavis@gmail.com>
2018-04-09 21:43:51 -0500

    defined LoadBBD

--- src/BeanieBaby.cpp
+++ src/BeanieBaby.cpp
@@ -3,8 +3,14 @@
 /**
  * Loads BeanieBaby data from provided url as a string.
  * @param url url to read data from.
- * @return string with url contents (i.e. json)
+ * @return string with url contents (i.e. json) or empty string on load fail
  */
 std::string LoadBeanieBabyData(std::string url) {
+    auto request = cpr::Url(url);
+    auto response = cpr::Get(request);
+    if (response.status_code == 200) {
+        return response.text;
+    }
 
+    return "";
 }
