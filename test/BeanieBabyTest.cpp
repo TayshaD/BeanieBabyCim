4993c2b35eacbfeb22f99343b5e6e5d4a53928e3
Taysha <taariyadavis@gmail.com>
2018-04-10 09:10:33 -0500

    begun preliminary test for Parse

--- test/BeanieBabyTest.cpp
+++ test/BeanieBabyTest.cpp
@@ -7,5 +7,10 @@ TEST_CASE("Loading data from url") {
 }
 
 TEST_CASE("Parsing beanie baby data from text") {
+   std::string raw_text = LoadBeanieBabyData();
+   std::vector<BeanieBaby> babies = ParseBeanieData(raw_text);
 
+   SECTION("All baby objects were loaded") {
+      REQUIRE(babies.size() == 45);
+   }
 }
