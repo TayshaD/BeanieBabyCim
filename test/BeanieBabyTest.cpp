7a850a78ebf2e87835a345a45c82335935ebdeef
Taysha <taariyadavis@gmail.com>
2018-04-10 12:45:33 -0500

    test cases for comparison, writing

--- test/BeanieBabyTest.cpp
+++ test/BeanieBabyTest.cpp
@@ -29,3 +29,17 @@ TEST_CASE("Copying a BeanieBaby object doesn't copy the copies_ field") {
 
     SECTION("Not specifying any # of copies results in copies_ = 1") {}
 }
+
+TEST_CASE("Comparison operator does not compare the copies_ field") {
+    BeanieBaby example_baby("Iggy example", 16);
+    BeanieBaby other_baby("Iggy example", 5);
+
+    REQUIRE(example_baby == other_baby);
+}
+
+TEST_CASE("<< operator prints nicely_formatted string") {
+    BeanieBaby example_baby("Iggy Example", 16);
+    std::ostringstream output;
+    output << example_baby;
+    REQUIRE(output.str() == "Iggy Example is a Beanie Baby worth $5.");
+}
