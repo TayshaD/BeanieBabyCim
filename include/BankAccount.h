faed9b259f44fbe8ddc59a7dda6b681529b35ebf
Taysha <taariyadavis@gmail.com>
2018-05-08 12:48:23 -0500

    beginning StorageBin

--- include/BankAccount.h
+++ include/BankAccount.h
@@ -1,11 +1,15 @@
+#ifndef BEANIEBABYSIM_BANKACCOUNT_H
+#define BEANIEBABYSIM_BANKACCOUNT_H
+
 #include <string>
 
 class BankAccount {
 	double balance_;
 
-	const std::string INSUFFICIENT_FUNDS = "You don't have enough money for that transaction.";
-
 public:
+
+	static const std::string INSUFFICIENT_FUNDS = "You don't have enough money for that transaction.";
+
 	explicit BankAccount(double starting_balance = 0);
 
 	void deposit(double amount);
@@ -16,3 +20,5 @@ public:
 
 	std::string info();
 };
+
+#endif //BEANIEBABYSIM_BANKACCOUNT_H
--- include/Simulation.h
+++ include/Simulation.h
@@ -1,10 +1,13 @@
- #define BEANIEBABYSIM_SIMULATION_H
+#ifndef BEANIEBABYSIM_SIMULATION_H
+#define BEANIEBABYSIM_SIMULATION_H
  
- #include <string>
-+#include "Context.h"
+#include <string>
+#include "Context.h"
  
-+class Simulation : public Context {
+class Simulation : public Context {
  
      Simulation();
-     
+
 };
+
+#endif //BEANIEBABYSIM_SIMULATION_H
